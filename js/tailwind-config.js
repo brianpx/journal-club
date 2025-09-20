1a366ac165e2d99c628cff39720daa297c4da276
Brian Pitts <bpitts@health.ucdavis.edu>
2025-09-20 16:57:49 +0700

    Update content path in Tailwind config

--- js/tailwind-config.js
+++ js/tailwind-config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./jc_guide.html", "./js/**/*.{js,ts}"],
+    content: ["journal-club/jc_guide.html", "./js/**/*.{js,ts}"],
     theme: { extend: {} },
     darkMode: "class", // works with your theme toggle
     plugins: [],
